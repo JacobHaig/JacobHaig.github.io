768324f38f86fa3d0117c415f183259ba70a88b8
Jacob Haig <jacobedwardhaig@gmail.com>
2017-10-31 18:12:05 -0400

    idk somehow

--- javascripts/mainNode2.js
+++ javascripts/mainNode2.js
@@ -5,46 +5,47 @@ var nodes = [];
 var framerate = 25;
 
 function createObject(x, y, mass) {
-    this.x = x;
-    this.y = y;
-    this.mass = mass;
+  this.x = x;
+  this.y = y;
+  this.mass = mass;
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    w = windowWidth;
-    h = windowHeight;
-	noStroke();
-    for (var i = 0; i < amount; i++) 
-		startNode();
+  w = windowWidth*.5;
+  h = windowHeight-50;
+  createCanvas(w, h);
+
+  noStroke();
+  for (var i = 0; i < amount; i++)
+    startNode();
 }
 
 function startNode() {
-    var Object = new createObject(random(0, w), random(0, h), random(1,10)); 
-    nodes.push(Object);
+  var Object = new createObject(random(0, w), random(0, h), random(1,10));
+  nodes.push(Object);
 }
 
 function windowResized() {
-	w = windowWidth;
-    h = windowHeight;
-	resizeCanvas(windowWidth, windowHeight);
+  w = windowWidth*.5;
+  h = windowHeight-50;
+  resizeCanvas(w, h);
 }
 
 function draw() {
-    clear();
-    nodes.forEach(moveUp);
-    nodes.forEach(drawNodes);
-    frameRate(framerate);
+  clear();
+  nodes.forEach(moveUp);
+  nodes.forEach(drawNodes);
+  frameRate(framerate);
 }
 
 function drawNodes(item, index) {
-    fill(255 - item.mass * 10);
-    ellipse(nodes[index].x, nodes[index].y, nodes[index].mass/2, nodes[index].mass/2);
+  fill(255 - item.mass * 10);
+  ellipse(nodes[index].x, nodes[index].y, nodes[index].mass/2, nodes[index].mass/2);
 }
 
 function moveUp(item, index) {
 	item.y -= item.mass/4;
 	if ( item.y < 0)  //if node goes too far out of range reset node
-        item.y += h;
-    
+    item.y += h;
+
 }
